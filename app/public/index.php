8b8060ad1630dc043cdb9c5c0222ac656d2f05e8
Magomedali <web-ali@yandex.ru>
2019-06-23 02:17:04 +0300

    connect to db and select test data

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,10 @@
 <?php
-phpinfo();
+
+$USER = "root";
+$PASS = "12345";
+$c = new \PDO("mysql:host=raport-mysql;dbname=testdb",$USER,$PASS);
+
+$stmt = $c->query("SELECT * FROM user");
+
+print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
 ?>
