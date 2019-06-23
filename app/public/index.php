d5d0d8c9bfcbf3bb8a60118f2265be95d8ed2bf4
Magomedali <web-ali@yandex.ru>
2019-06-23 11:36:51 +0300

    work env on docker

--- app/public/index.php
+++ app/public/index.php
@@ -1,10 +1,4 @@
 <?php
 
-$USER = "root";
-$PASS = "12345";
-$c = new \PDO("mysql:host=raport-mysql;dbname=testdb",$USER,$PASS);
-
-$stmt = $c->query("SELECT * FROM user");
-
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
+phpinfo();
 ?>
